b3a800f1295bb9b67b1a9f388ac8347923b9178a
srikanthsc <79998871+srikanthsc@users.noreply.github.com>
2022-01-16 17:45:30 +0100

    training ++

--- blockchain2.sol
+++ blockchain2.sol
@@ -4,40 +4,54 @@ contract gfgMathPlus
 {
     // Declaring the state
     // variables
-    uint firstNo ;
-    uint secondNo ;
- 
+    uint firstNo;
+
+    uint secondNo;
+
     // Defining the function
     // to set the value of the
     // first variable
-   function fistnum(uint x) public{
-    firstNo = x;
-   }
 
-   function helloworld() pure public returns(string memory){
+    function fistnum(uint x) public{
+        firstNo = x;
+    }
+    function helloworld() pure public returns(string memory){
+    
     return "hello world";
-   }
-
-   function secondnum(uint y) public{
-    secondNo = y;
-   }
-   function test() public{
-    console.log("bonjour");
-    console.log("test transaction in the blockchain");
-     
-   }
-
-   function addothermethod() view public returns(uint){
-       uint num1 = 12;
-       uint num2 = 12;
-       uint sum = num1 + num2;
-       console.log("the result of the sum is:>");
-       console.log(sum);
-       return sum;
-   }
-
-   function add() view public returns(uint) {
-    uint sum = firstNo + secondNo;
+    }
+    function secondnum(uint y) public{
+        secondNo = y;
+    }
+    function test() public{
+        console.log("bonjour");
+        console.log("test transaction in the blockchain");
+    }
+    function introblock() public {
+        console.log("my super blockchain");
+    }
+    function addothermethod() view public returns(uint){
+        uint num1 = 12;
+        console.log(num1);
+        uint num2 = 12;
+        console.log(num2);
+        uint sum = num1 + num2;
+        console.log("the result of the sum is:>");
+        console.log(sum);
     return sum;
-   }
+    }
+
+
+    function add() view public returns(uint) {
+        uint sum = firstNo + secondNo;
+    return sum;
+    }
+
+
+    function min() view public returns(uint) {
+        uint min = firstNo - secondNo;
+        console.log("display the result in the console");
+        console.log(min);
+    return min;
+    }
+
 }
